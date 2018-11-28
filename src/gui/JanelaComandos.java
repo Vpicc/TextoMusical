0dd5e199b736380e9eee0071deacf9ee2dc759dc
Victor Piccoli <victorpicc@gmail.com>
2018-11-28 12:52:08 -0200

    correção charset

--- src/gui/JanelaComandos.java
+++ src/gui/JanelaComandos.java
@@ -18,23 +18,23 @@ public class JanelaComandos extends Janela{
 	}
 	
 	private String textoComandos() {
-		return "<html><meta http-equiv='content-type' content='text/html;charset=utf-8â€³'><table border=\"1\"><tr><th>Texto</th><th>AÃ§Ã£o</th></tr>"
-				+ "<tr><td>A (maiÃºsculo)</td><td>Nota LÃ¡</td></tr>"
-				+ "<tr><td>B (maiÃºsculo)</td><td>Nota Si</td></tr>"
-				+ "<tr><td>C (maiÃºsculo)</td><td>Nota DÃ³</td></tr>"
-				+ "<tr><td>D (maiÃºsculo)</td><td>Nota RÃ©</td></tr>"
-				+ "<tr><td>E (maiÃºsculo)</td><td>Nota Mi</td></tr>"
-				+ "<tr><td>F (maiÃºsculo)</td><td>Nota FÃ¡</td></tr>"
-				+ "<tr><td>G (maiÃºsculo)</td><td>Nota Sol</td></tr>"
-				+ "<tr><td>EspaÃ§o</td><td>Aumenta o volume para o DOBRO do volume</td></tr>"
+		return "<html><meta http-equiv='content-type' content='text/html;charset=utf-8'><table border=\"1\"><tr><th>Texto</th><th>Ação</th></tr>"
+				+ "<tr><td>A (maiúsculo)</td><td>Nota Lá</td></tr>"
+				+ "<tr><td>B (maiúsculo)</td><td>Nota Si</td></tr>"
+				+ "<tr><td>C (maiúsculo)</td><td>Nota Dó</td></tr>"
+				+ "<tr><td>D (maiúsculo)</td><td>Nota Ré</td></tr>"
+				+ "<tr><td>E (maiúsculo)</td><td>Nota Mi</td></tr>"
+				+ "<tr><td>F (maiúsculo)</td><td>Nota Fá</td></tr>"
+				+ "<tr><td>G (maiúsculo)</td><td>Nota Sol</td></tr>"
+				+ "<tr><td>Espaço</td><td>Aumenta o volume para o DOBRO do volume</td></tr>"
 				+ "<tr><td>!</td><td>Troca instrumento para Harpsichord</td></tr>"
 				+ "<tr><td>O ou o,I ou i,U ou u</td><td>Aumenta volume em 10%</td></tr>"
-				+ "<tr><td>DÃ­gito par ou impar</td><td>Troca instrumento para o General MIDI cujo Numero Ã© igual ao valor do instrumento ATUAL + valor do dÃ­gito</td></tr>"
-				+ "<tr><td>? e .</td><td>Aumenta UMA oitava; Se nÃ£o puder aumentar, volta para a default</td></tr>"
+				+ "<tr><td>Dígito par ou impar</td><td>Troca instrumento para o General MIDI cujo numero é igual ao valor do instrumento ATUAL + valor do dígito</td></tr>"
+				+ "<tr><td>? e .</td><td>Aumenta UMA oitava; Se não puder aumentar, volta para a default</td></tr>"
 				+ "<tr><td>NL (Nova Linha)</td><td>Troca instrumento para Tubular Bells</td></tr>"
 				+ "<tr><td>;</td><td>Troca Instrumento para Pan Flute</td></tr>"
 				+ "<tr><td>,</td><td>Troca Instrumento para Church Organ</td></tr>"
-				+ "<tr><td>Outros caracteres</td><td>Se caractere anterior era NOTA(A a G) repete a nota; Caso contrÃ¡rio silÃªncio</td></tr>"
+				+ "<tr><td>Outros caracteres</td><td>Se caractere anterior era NOTA(A a G) repete a nota; Caso contrário silêncio</td></tr>"
 				+ "</table></html>";
 	}
 }
